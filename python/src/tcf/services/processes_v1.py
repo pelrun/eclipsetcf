eb07c3cee93b77241a9d1bdfd6e0b79362f994b8
Frederic Leger <frederic.leger@windriver.com>
2012-08-06 15:53:30 +0200

    TCF Python: Bug 386552 - Missing processes properties in processes_v1.py

--- python/src/tcf/services/processes_v1.py
+++ python/src/tcf/services/processes_v1.py
@@ -1,5 +1,5 @@
-# *******************************************************************************
-# * Copyright (c) 2011 Wind River Systems, Inc. and others.
+# *****************************************************************************
+# * Copyright (c) 2011, 2012 Wind River Systems, Inc. and others.
 # * All rights reserved. This program and the accompanying materials
 # * are made available under the terms of the Eclipse Public License v1.0
 # * which accompanies this distribution, and is available at
@@ -7,7 +7,7 @@
 # *
 # * Contributors:
 # *     Wind River Systems - initial API and implementation
-# *******************************************************************************
+# *****************************************************************************
 
 """
 Extension of Processes service.
@@ -23,24 +23,34 @@ NAME = "ProcessesV1"
 START_ATTACH = "Attach"
 # Boolean, auto-attach process children
 START_ATTACH_CHILDREN = "AttachChildren"
+# Boolean, stop at process entry
+START_STOP_AT_ENTRY = "StopAtEntry"
+# Boolean, stop at main()
+START_STOP_AT_MAIN = "StopAtMain"
 # Boolean, Use pseudo-terminal for the process standard I/O
 START_USE_TERMINAL = "UseTerminal"
+
 
 class ProcessesV1Service(processes.ProcessesService):
     def getName(self):
         return NAME
 
-    def start(self, directory, file, command_line, environment, params, done):
+    def start(self, directory, filePath, command_line, environment, params,
+              done):
         """
         Start a new process on remote machine.
         @param directory - initial value of working directory for the process.
-        @param file - process image file.
+        @param filePath - process image file.
         @param command_line - command line arguments for the process.
-        Note: the service does NOT add image file name as first argument for the process.
-        If a client wants first parameter to be the file name, it should add it itself.
+                              Note: the service does NOT add image file name as
+                              first argument for the process. If a client wants
+                              first parameter to be the file name, it should
+                              add it itself.
         @param environment - map of environment variables for the process,
-        if None then default set of environment variables will be used.
-        @param params - additional process start parameters as map, see START_* for supported keys.
+                             if None then default set of environment variables
+                             will be used.
+        @param params - additional process start parameters as map, see START_*
+                        for supported keys.
         @param done - call back interface called when operation is completed.
         @return pending command handle, can be used to cancel the command.
         """
