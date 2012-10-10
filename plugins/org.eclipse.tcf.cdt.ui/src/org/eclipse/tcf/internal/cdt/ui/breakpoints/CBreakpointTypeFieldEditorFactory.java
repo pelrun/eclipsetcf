54e76c44189d1d9583d97b2c4db85f464d85d7a0
Pawel Piech <pawel.piech@windriver.com>
2012-10-10 15:15:21 -0700

    Bug 391610 - [breakpoints] Add "Temporary" checkbox to breakpoint properties dialog.

--- plugins/org.eclipse.tcf.cdt.ui/src/org/eclipse/tcf/internal/cdt/ui/breakpoints/CBreakpointTypeFieldEditorFactory.java
+++ plugins/org.eclipse.tcf.cdt.ui/src/org/eclipse/tcf/internal/cdt/ui/breakpoints/CBreakpointTypeFieldEditorFactory.java
@@ -10,18 +10,31 @@
  *******************************************************************************/
 package org.eclipse.tcf.internal.cdt.ui.breakpoints;
 
+import org.eclipse.cdt.debug.core.model.ICBreakpointType;
 import org.eclipse.cdt.debug.ui.breakpoints.IFieldEditorFactory;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.tcf.services.IBreakpoints;
 
 
 /**
  * HardwareFieldEditorFactory - Create the field editor for hardware breakpoint support.
  */
-public class HardwareFieldEditorFactory implements IFieldEditorFactory {
+public class CBreakpointTypeFieldEditorFactory implements IFieldEditorFactory {
+    
+    public final static String NAME_HARDWARE = "org.eclipse.tcf.cdt.Hardware";
+    public final static String NAME_TEMPORARY = "org.eclipse.tcf.cdt.Temporary";
+
+    public final static String LABEL_HARDWARE = "Hardware";
+    public final static String LABEL_TEMPORARY = "Temporary";
 
     public FieldEditor createFieldEditor(String name, String labelText, Composite parent) {
-        return new HardwareFieldEditor (parent);
+        if (NAME_HARDWARE.equals(name)) {
+            return new CBreakpointTypeFieldEditor (parent, LABEL_HARDWARE, ICBreakpointType.HARDWARE, IBreakpoints.CAPABILITY_BREAKPOINT_TYPE);
+        } else if (NAME_TEMPORARY.equals(name)) {
+            return new CBreakpointTypeFieldEditor (parent, LABEL_TEMPORARY, ICBreakpointType.TEMPORARY, IBreakpoints.CAPABILITY_TEMPORARY);
+        }
+        return null;
     }
 
 
