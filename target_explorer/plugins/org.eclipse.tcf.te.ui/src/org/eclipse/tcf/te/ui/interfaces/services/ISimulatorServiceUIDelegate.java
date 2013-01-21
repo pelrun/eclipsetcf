e96b8f62036f90ee2c0a1f09775528925e164278
Uwe Stieber <uwe.stieber@windriver.com>
2013-01-21 09:40:18 +0100

    Target Explorer: Added basic simulator service UI delegate interface

---
+++ target_explorer/plugins/org.eclipse.tcf.te.ui/src/org/eclipse/tcf/te/ui/interfaces/services/ISimulatorServiceUIDelegate.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Wind River Systems, Inc. and others. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Wind River Systems - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tcf.te.ui.interfaces.services;
+
+import org.eclipse.tcf.te.runtime.services.interfaces.ISimulatorService;
+
+/**
+ * Simulator service UI delegate.
+ */
+public interface ISimulatorServiceUIDelegate {
+
+	/**
+	 * Get the simulator service the UI delegate is associated with.
+	 *
+	 * @return The simulator service.
+	 */
+	public ISimulatorService getService();
+
+	/**
+	 * Get the name of the simulator service to identify the simulator (type)
+	 * to the user in the UI.
+	 *
+	 * @return The simulator service name.
+	 */
+	public String getName();
+}
