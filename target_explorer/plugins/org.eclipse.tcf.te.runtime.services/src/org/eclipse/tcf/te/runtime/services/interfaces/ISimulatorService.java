f3bce3e8b9d96f528b03bc4ae7fa2930d760ad0d
Uwe Stieber <uwe.stieber@windriver.com>
2013-01-22 11:14:31 +0100

    Target Explorer: Adjusted simulator service API definition

--- target_explorer/plugins/org.eclipse.tcf.te.runtime.services/src/org/eclipse/tcf/te/runtime/services/interfaces/ISimulatorService.java
+++ target_explorer/plugins/org.eclipse.tcf.te.runtime.services/src/org/eclipse/tcf/te/runtime/services/interfaces/ISimulatorService.java
@@ -11,7 +11,6 @@ package org.eclipse.tcf.te.runtime.services.interfaces;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.tcf.te.runtime.interfaces.callback.ICallback;
-import org.eclipse.tcf.te.runtime.interfaces.properties.IPropertiesContainer;
 
 /**
  * Simulator service.
@@ -24,55 +23,33 @@ import org.eclipse.tcf.te.runtime.interfaces.properties.IPropertiesContainer;
 public interface ISimulatorService extends IService {
 
 	/**
-	 * Configure the simulator service instance.
-	 * <p>
-	 * The properties to set depends on the simulator service instance. Check
-	 * the documentation of the simulator service instance implementation for
-	 * details. If the given configuration is <code>null</code>, the simulator
-	 * service instance should reset the configuration to it's defaults.
-	 *
-	 * @param config The simulator service configuration or <code>null</code>.
-	 */
-	public void setConfiguration(IPropertiesContainer config);
-
-	/**
-	 * Returns the simulator service instance configuration.
-	 *
-	 * @return The simulator service instance configuration.
-	 */
-	public IPropertiesContainer getConfiguration();
-
-	/**
 	 * Starts the simulator.
-	 * <p>
-	 * The simulation properties are retrieved from the given context object.
 	 *
 	 * @param context The context. Must not be <code>null</code>.
+	 * @param settings The encoded simulator settings or <code>null</code>.
 	 * @param callback The callback to invoke once the operation finishes. Must not be <code>null</code>.
 	 * @param monitor The progress monitor or <code>null</code>.
 	 */
-	public void start(Object context, ICallback callback, IProgressMonitor monitor);
+	public void start(Object context, String settings, ICallback callback, IProgressMonitor monitor);
 
 	/**
 	 * Stops the simulator.
-	 * <p>
-	 * The simulation properties are retrieved from the given context object.
 	 *
 	 * @param context The context. Must not be <code>null</code>.
+	 * @param settings The encoded simulator settings or <code>null</code>.
 	 * @param callback The callback to invoke once the operation finishes. Must not be <code>null</code>.
 	 * @param monitor The progress monitor or <code>null</code>.
 	 */
-	public void stop(Object context, ICallback callback, IProgressMonitor monitor);
+	public void stop(Object context, String settings, ICallback callback, IProgressMonitor monitor);
 
 	/**
 	 * Checks if the simulator is running.
 	 * <p>
-	 * The simulation properties are retrieved from the given context object.
-	 * <p>
 	 * The result of the check is return as {@link Boolean} object by the callback's {@link ICallback#getResult()} method.
 	 *
 	 * @param context The context. Must not be <code>null</code>.
+	 * @param settings The encoded simulator settings or <code>null</code>.
 	 * @param callback The callback to invoke once the operation finishes. Must not be <code>null</code>.
 	 */
-	public void isRunning(Object context, ICallback callback);
+	public void isRunning(Object context, String settings, ICallback callback);
 }
