744d08307dede3a84db9fe1a941825f6d2c74a68
Eugene Tarassov <eugene.tarassov@xilinx.com>
2013-06-14 08:05:03 -0700

    TCF Tests: fixed a typo in code comment

--- plugins/org.eclipse.tcf.debug/src/org/eclipse/tcf/internal/debug/tests/Main.java
+++ plugins/org.eclipse.tcf.debug/src/org/eclipse/tcf/internal/debug/tests/Main.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Wind River Systems, Inc. and others.
+ * Copyright (c) 2008, 2013 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,7 +23,7 @@ import org.eclipse.tcf.protocol.IPeer;
 import org.eclipse.tcf.protocol.Protocol;
 
 /**
- * This class is user to run TCF test suite from command line.
+ * This class is used to run TCF test suite from command line.
  */
 public class Main {
 
