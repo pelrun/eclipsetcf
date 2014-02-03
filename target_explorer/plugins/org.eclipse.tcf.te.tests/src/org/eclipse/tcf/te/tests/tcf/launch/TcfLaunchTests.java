b98dfa98937ea6b9cdb6171142bd573bc7f8df9a
Uwe Stieber <uwe.stieber@windriver.com>
2014-02-03 16:09:18 +0100

    Target Explorer: Fix findbugs warnings

--- target_explorer/plugins/org.eclipse.tcf.te.tests/src/org/eclipse/tcf/te/tests/tcf/launch/TcfLaunchTests.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tests/src/org/eclipse/tcf/te/tests/tcf/launch/TcfLaunchTests.java
@@ -161,8 +161,8 @@ public class TcfLaunchTests extends TcfTestCase {
 			outputChannel = new FileOutputStream(dest).getChannel();
 			outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
 		} finally {
-			inputChannel.close();
-			outputChannel.close();
+			if (inputChannel != null) inputChannel.close();
+			if (outputChannel != null) outputChannel.close();
 		}
 	}
 
