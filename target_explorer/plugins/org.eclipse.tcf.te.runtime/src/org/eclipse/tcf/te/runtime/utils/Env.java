84d7f1e54017511eb7de145b4a53cac28bb611bb
Uwe Stieber <uwe.stieber@windriver.com>
2014-02-12 08:40:54 +0100

    Target Explorer: Environment variables without values ignored on Windows only

--- target_explorer/plugins/org.eclipse.tcf.te.runtime/src/org/eclipse/tcf/te/runtime/utils/Env.java
+++ target_explorer/plugins/org.eclipse.tcf.te.runtime/src/org/eclipse/tcf/te/runtime/utils/Env.java
@@ -90,11 +90,11 @@ public class Env {
 					if ("".equals(name.trim()) || name.contains("=") || name.contains(":")) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 						continue;
 					}
+					// If the variable has no value, ignore the variable
+					if (parts.length < 2) continue;
 				}
-				// If the variable has no value, ignore the variable
-				if (parts.length < 2) continue;
 				// Get the variable value
-				String value = parts[1].trim();
+				String value = parts.length > 1 ? parts[1].trim() : ""; //$NON-NLS-1$
 				// Don't overwrite the TERM variable if in terminal mode
 				if (terminal && "TERM".equals(name)) continue; //$NON-NLS-1$
 				// If a variable with the name does not exist, just append it
