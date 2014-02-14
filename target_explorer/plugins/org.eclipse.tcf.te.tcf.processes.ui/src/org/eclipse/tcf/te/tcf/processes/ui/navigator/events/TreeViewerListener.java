504eb2ecf6736587fb0cedb536aeb6d5a5645318
Uwe Stieber <uwe.stieber@windriver.com>
2014-02-14 08:38:50 +0100

    Target Explorer: Fix potential reentrant viewer entry issue on tree expanded event

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.processes.ui/src/org/eclipse/tcf/te/tcf/processes/ui/navigator/events/TreeViewerListener.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.processes.ui/src/org/eclipse/tcf/te/tcf/processes/ui/navigator/events/TreeViewerListener.java
@@ -84,13 +84,15 @@ public class TreeViewerListener implements ITreeViewerListener {
     			PendingOperationModelNode pendingNode = new PendingOperationNode();
     			pendingNode.setParent(node);
     			refreshable.setPendingOperationNode(pendingNode);
-				// Trigger a refresh of the view content.
-				ChangeEvent ev = new ChangeEvent(node, IContainerModelNode.NOTIFY_CHANGED, null, null);
-				EventManager.getInstance().fireEvent(ev);
 
     			Runnable runnable = new Runnable() {
     				@Override
     				public void run() {
+    					// Trigger a refresh of the view content.
+    					ChangeEvent ev = new ChangeEvent(node, IContainerModelNode.NOTIFY_CHANGED, null, null);
+    					EventManager.getInstance().fireEvent(ev);
+
+    					// Get the parent model of the node
     					IModel model = node.getParent(IModel.class);
     					Assert.isNotNull(model);
 
