49824c0cd3060498f0dc7988ef865b4b2f48d3d3
Uwe Stieber <uwe.stieber@windriver.com>
2014-04-25 17:35:54 +0200

    Releng: Fix copyrights

--- target_explorer/plugins/org.eclipse.tcf.te.runtime.persistence/src/org/eclipse/tcf/te/runtime/persistence/utils/DataHelper.java
+++ target_explorer/plugins/org.eclipse.tcf.te.runtime.persistence/src/org/eclipse/tcf/te/runtime/persistence/utils/DataHelper.java
@@ -2,7 +2,7 @@
  * DataHelper.java
  * Created on Aug 21, 2013
  *
- * Copyright (c) 2013 Wind River Systems, Inc.
+ * Copyright (c) 2013, 2014 Wind River Systems, Inc.
  *
  * The right to copy, distribute, modify, or otherwise make use
  * of this software may be licensed only pursuant to the terms
--- target_explorer/plugins/org.eclipse.tcf.te.runtime.statushandler/src/org/eclipse/tcf/te/runtime/statushandler/StatusHandlerUtil.java
+++ target_explorer/plugins/org.eclipse.tcf.te.runtime.statushandler/src/org/eclipse/tcf/te/runtime/statushandler/StatusHandlerUtil.java
@@ -2,7 +2,7 @@
  * StatusHandlerUtil.java
  * Created on May 21, 2012
  *
- * Copyright (c) 2012, 2013 Wind River Systems, Inc.
+ * Copyright (c) 2012, 2014 Wind River Systems, Inc.
  *
  * The right to copy, distribute, modify, or otherwise make use
  * of this software may be licensed only pursuant to the terms
