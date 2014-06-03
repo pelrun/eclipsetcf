d276d951c0e3670306744080e15680d1aaf7fe7f
Uwe Stieber <uwe.stieber@windriver.com>
2014-06-03 10:39:24 +0200

    Target Explorer: Deal with InterruptedException from Protocol.invokeAndWait on shutdown

--- target_explorer/plugins/org.eclipse.tcf.te.core/src/org/eclipse/tcf/te/core/properties/PropertyTester.java
+++ target_explorer/plugins/org.eclipse.tcf.te.core/src/org/eclipse/tcf/te/core/properties/PropertyTester.java
@@ -24,88 +24,94 @@ public class PropertyTester extends org.eclipse.core.expressions.PropertyTester 
 	 */
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		IAdapterManager manager = Platform.getAdapterManager();
-		if (manager == null) return false;
+		try {
+			IAdapterManager manager = Platform.getAdapterManager();
+			if (manager == null) return false;
 
-		// "hasAdapter": Checks if the adapter given by the arguments is registered for the given receiver
-		if ("hasAdapter".equals(property)) { //$NON-NLS-1$
-			// The class to adapt to is within the expected value
-			String adapterType = expectedValue instanceof String ? (String)expectedValue : null;
-			if (adapterType != null) {
-				return manager.hasAdapter(receiver, adapterType);
+			// "hasAdapter": Checks if the adapter given by the arguments is registered for the given receiver
+			if ("hasAdapter".equals(property)) { //$NON-NLS-1$
+				// The class to adapt to is within the expected value
+				String adapterType = expectedValue instanceof String ? (String)expectedValue : null;
+				if (adapterType != null) {
+					return manager.hasAdapter(receiver, adapterType);
+				}
 			}
-		}
-		if ("canAdaptTo".equals(property)) { //$NON-NLS-1$
-			// Read the arguments and look for "forceAdapterLoad"
-			boolean forceAdapterLoad = false;
-			for (Object arg : args) {
-				if (arg instanceof String && "forceAdapterLoad".equalsIgnoreCase((String)arg)) { //$NON-NLS-1$
-					forceAdapterLoad = true;
+			if ("canAdaptTo".equals(property)) { //$NON-NLS-1$
+				// Read the arguments and look for "forceAdapterLoad"
+				boolean forceAdapterLoad = false;
+				for (Object arg : args) {
+					if (arg instanceof String && "forceAdapterLoad".equalsIgnoreCase((String)arg)) { //$NON-NLS-1$
+						forceAdapterLoad = true;
+					}
+				}
+
+				// The class to adapt to is within the expected value
+				String adapterType = expectedValue instanceof String ? (String)expectedValue : null;
+				if (adapterType != null) {
+					Object adapter = manager.getAdapter(receiver, adapterType);
+					if (adapter != null) return true;
+
+					// No adapter. This can happen too if the plug-in contributing the adapter
+					// factory hasn't been loaded yet.
+					if (forceAdapterLoad) adapter = manager.loadAdapter(receiver, adapterType);
+					if (adapter != null) return true;
 				}
 			}
 
-			// The class to adapt to is within the expected value
-			String adapterType = expectedValue instanceof String ? (String)expectedValue : null;
-			if (adapterType != null) {
-				Object adapter = manager.getAdapter(receiver, adapterType);
-				if (adapter != null) return true;
+			// "hasEnvVar" tests for the existence of a system property or environment variable
+			// with the name passed in via the arguments.
+			if ("hasEnvVar".equals(property) && expectedValue instanceof Boolean) { //$NON-NLS-1$
+				boolean hasEnvVar = false;
 
-				// No adapter. This can happen too if the plug-in contributing the adapter
-				// factory hasn't been loaded yet.
-				if (forceAdapterLoad) adapter = manager.loadAdapter(receiver, adapterType);
-				if (adapter != null) return true;
-			}
-		}
-
-		// "hasEnvVar" tests for the existence of a system property or environment variable
-		// with the name passed in via the arguments.
-		if ("hasEnvVar".equals(property) && expectedValue instanceof Boolean) { //$NON-NLS-1$
-			boolean hasEnvVar = false;
-
-			String name = null;
-			for (Object arg : args) {
-				if (arg instanceof String) {
-					name = (String)arg;
-					break;
+				String name = null;
+				for (Object arg : args) {
+					if (arg instanceof String) {
+						name = (String)arg;
+						break;
+					}
 				}
-			}
 
-			if (name != null) {
-				String value = System.getProperty(name);
-				if (value == null) value = System.getenv(name);
-				hasEnvVar = value != null;
-			}
-
-			return hasEnvVar == ((Boolean)expectedValue).booleanValue();
-		}
-
-		// "envVar" tests for the value of a system property or environment variable
-		// with the name passed in via the arguments.
-		if ("envVar".equals(property)) { //$NON-NLS-1$
-			String value = null;
-
-			String name = null;
-			for (Object arg : args) {
-				if (arg instanceof String) {
-					name = (String)arg;
-					break;
+				if (name != null) {
+					String value = System.getProperty(name);
+					if (value == null) value = System.getenv(name);
+					hasEnvVar = value != null;
 				}
+
+				return hasEnvVar == ((Boolean)expectedValue).booleanValue();
 			}
 
-			if (name != null) {
-				value = System.getProperty(name);
-				if (value == null) value = System.getenv(name);
+			// "envVar" tests for the value of a system property or environment variable
+			// with the name passed in via the arguments.
+			if ("envVar".equals(property)) { //$NON-NLS-1$
+				String value = null;
+
+				String name = null;
+				for (Object arg : args) {
+					if (arg instanceof String) {
+						name = (String)arg;
+						break;
+					}
+				}
+
+				if (name != null) {
+					value = System.getProperty(name);
+					if (value == null) value = System.getenv(name);
+				}
+
+				// Always check against the string value
+				return value != null ? expectedValue.toString().equals(value) : false;
 			}
 
-			// Always check against the string value
-			return value != null ? expectedValue.toString().equals(value) : false;
-		}
-
-		if ("isConnectStateChangeActionAllowed".equals(property) && receiver instanceof IConnectable && expectedValue instanceof String) { //$NON-NLS-1$
-			return ((IConnectable)receiver).isConnectStateChangeActionAllowed(ConnectStateHelper.getConnectAction((String)expectedValue));
-		}
-		if ("isConnectState".equals(property) && receiver instanceof IConnectable && expectedValue instanceof String) { //$NON-NLS-1$
-			return ((IConnectable)receiver).getConnectState() == ConnectStateHelper.getConnectState((String)expectedValue);
+			if ("isConnectStateChangeActionAllowed".equals(property) && receiver instanceof IConnectable && expectedValue instanceof String) { //$NON-NLS-1$
+				return ((IConnectable)receiver).isConnectStateChangeActionAllowed(ConnectStateHelper.getConnectAction((String)expectedValue));
+			}
+			if ("isConnectState".equals(property) && receiver instanceof IConnectable && expectedValue instanceof String) { //$NON-NLS-1$
+				return ((IConnectable)receiver).getConnectState() == ConnectStateHelper.getConnectState((String)expectedValue);
+			}
+		} catch (Error e) {
+			if (!(e.getCause() instanceof InterruptedException)) {
+				throw e;
+			}
 		}
 
 	    return false;
