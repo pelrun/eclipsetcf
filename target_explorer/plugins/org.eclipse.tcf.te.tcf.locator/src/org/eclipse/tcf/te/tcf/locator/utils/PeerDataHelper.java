2bace60b015cb7dc35caa255a84abd89f87773c2
Tobias Schwarz <tobias.schwarz@windriver.com>
2014-06-11 07:41:26 +0200

    Target Explorer: add encode/decode for list of peers

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.locator/src/org/eclipse/tcf/te/tcf/locator/utils/PeerDataHelper.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.locator/src/org/eclipse/tcf/te/tcf/locator/utils/PeerDataHelper.java
@@ -63,4 +63,45 @@ public class PeerDataHelper {
 		}
 		return new IPeerNode[0];
 	}
+
+	/**
+	 * Encode an array of peers to a string.
+	 * @param contexts The array of peers.
+	 * @return String representing the array of peers.
+	 */
+	public static final String encodePeerList(IPeer[] contexts) {
+		try {
+			if (contexts != null && contexts.length > 0) {
+				IPersistenceDelegate delegate = PersistenceManager.getInstance().getDelegate(IPeer.class, String.class);
+				return (String)delegate.writeList(contexts, String.class);
+			}
+		}
+		catch (Exception e) {
+		}
+		return null;
+	}
+
+	/**
+	 * Decode a string encoded array of peers.
+	 * @param encoded The string encoded peers.
+	 * @return Array of peers.
+	 */
+	public static final IPeer[] decodePeerList(String encoded) {
+		try {
+			if (encoded != null && encoded.trim().length() > 0) {
+				IPersistenceDelegate delegate = PersistenceManager.getInstance().getDelegate(IPeer.class, String.class);
+				Object[] input = delegate.readList(IPeer.class, encoded);
+				List<IPeer> peers = new ArrayList<IPeer>();
+				for (Object object : input) {
+					if (object instanceof IPeer) {
+						peers.add((IPeer)object);
+					}
+				}
+				return peers.toArray(new IPeer[peers.size()]);
+			}
+		}
+		catch (Exception e) {
+		}
+		return new IPeer[0];
+	}
 }
