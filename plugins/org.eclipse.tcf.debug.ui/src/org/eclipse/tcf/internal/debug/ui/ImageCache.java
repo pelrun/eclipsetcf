e2df43dc40c53798bfacbcc8ccbe3a85ee6470da
Eugene Tarassov <eugene.tarassov@xilinx.com>
2014-07-06 12:13:19 -0700

    TCF Debugger: ImageCache changed to search PNG files first, and use GIF if PNG is not found

--- plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/ImageCache.java
+++ plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/ImageCache.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 Wind River Systems, Inc. and others.
+ * Copyright (c) 2008, 2014 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.CompositeImageDescriptor;
@@ -28,55 +29,55 @@ import org.osgi.framework.Bundle;
 public class ImageCache {
 
     public static final String
-        IMG_TCF = "icons/tcf.gif",
-        IMG_TARGET_TAB = "icons/target_tab.gif",
-        IMG_DOWNLOAD_TAB = "icons/download_tab.gif",
-        IMG_TARGET_WIZARD = "icons/full/wizban/debug_wiz.png",
-        IMG_APPLICATION_TAB = "icons/application_tab.gif",
-        IMG_ARGUMENTS_TAB = "icons/arguments_tab.gif",
-        IMG_ATTRIBUTE = "icons/attribute.gif",
-        IMG_PATH = "icons/path.gif",
+        IMG_TCF = "icons/tcf",
+        IMG_TARGET_TAB = "icons/target_tab",
+        IMG_DOWNLOAD_TAB = "icons/download_tab",
+        IMG_TARGET_WIZARD = "icons/full/wizban/debug_wiz",
+        IMG_APPLICATION_TAB = "icons/application_tab",
+        IMG_ARGUMENTS_TAB = "icons/arguments_tab",
+        IMG_ATTRIBUTE = "icons/attribute",
+        IMG_PATH = "icons/path",
 
-        IMG_THREAD_TERMINATED = "icons/full/obj16/threadt_obj.gif",
-        IMG_THREAD_SUSPENDED = "icons/full/obj16/threads_obj.gif",
-        IMG_THREAD_RUNNNIG = "icons/full/obj16/thread_obj.gif",
-        IMG_THREAD_REVERSING = "icons/thread_reversing.gif",
-        IMG_THREAD_NOT_ACTIVE = "icons/thread_not_active.gif",
-        IMG_THREAD_UNKNOWN_STATE = "icons/thread_not_active.gif",
+        IMG_THREAD_TERMINATED = "icons/full/obj16/threadt_obj",
+        IMG_THREAD_SUSPENDED = "icons/full/obj16/threads_obj",
+        IMG_THREAD_RUNNNIG = "icons/full/obj16/thread_obj",
+        IMG_THREAD_REVERSING = "icons/thread_reversing",
+        IMG_THREAD_NOT_ACTIVE = "icons/thread_not_active",
+        IMG_THREAD_UNKNOWN_STATE = "icons/thread_not_active",
 
-        IMG_PROCESS_TERMINATED = "icons/full/obj16/debugtt_obj.gif",
-        IMG_PROCESS_SUSPENDED = "icons/full/obj16/debugts_obj.gif",
-        IMG_PROCESS_RUNNING = "icons/full/obj16/debugt_obj.gif",
+        IMG_PROCESS_TERMINATED = "icons/full/obj16/debugtt_obj",
+        IMG_PROCESS_SUSPENDED = "icons/full/obj16/debugts_obj",
+        IMG_PROCESS_RUNNING = "icons/full/obj16/debugt_obj",
 
-        IMG_REGISTER = "icons/full/obj16/genericregister_obj.gif",
+        IMG_REGISTER = "icons/full/obj16/genericregister_obj",
 
-        IMG_VARIABLE = "icons/var_simple.gif",
-        IMG_VARIABLE_POINTER = "icons/var_pointer.gif",
-        IMG_VARIABLE_AGGREGATE = "icons/var_aggr.gif",
+        IMG_VARIABLE = "icons/var_simple",
+        IMG_VARIABLE_POINTER = "icons/var_pointer",
+        IMG_VARIABLE_AGGREGATE = "icons/var_aggr",
 
-        IMG_NEW_EXPRESSION = "icons/full/elcl16/monitorexpression_tsk.gif",
+        IMG_NEW_EXPRESSION = "icons/full/elcl16/monitorexpression_tsk",
 
-        IMG_SIGNALS = "icons/signals.gif",
-        IMG_MEMORY_MAP = "icons/memory-map.gif",
-        IMG_PROFILER = "icons/profiler.png",
+        IMG_SIGNALS = "icons/signals",
+        IMG_MEMORY_MAP = "icons/memory-map",
+        IMG_PROFILER = "icons/profiler",
 
-        IMG_ARRAY_PARTITION = "icons/full/obj16/arraypartition_obj.gif",
+        IMG_ARRAY_PARTITION = "icons/full/obj16/arraypartition_obj",
 
-        IMG_STACK_FRAME_SUSPENDED = "icons/full/obj16/stckframe_obj.gif",
-        IMG_STACK_FRAME_RUNNING = "icons/full/obj16/stckframe_running_obj.gif",
-        IMG_STACK_FRAME_REVERSING = "icons/stckframe_reversing.gif",
+        IMG_STACK_FRAME_SUSPENDED = "icons/full/obj16/stckframe_obj",
+        IMG_STACK_FRAME_RUNNING = "icons/full/obj16/stckframe_running_obj",
+        IMG_STACK_FRAME_REVERSING = "icons/stckframe_reversing",
 
-        IMG_BREAKPOINT_ENABLED = "icons/full/obj16/brkp_obj.gif",
-        IMG_BREAKPOINT_DISABLED = "icons/full/obj16/brkpd_obj.gif",
-        IMG_BREAKPOINT_INSTALLED = "icons/ovr16/installed_ovr.gif",
-        IMG_BREAKPOINT_CONDITIONAL = "icons/ovr16/conditional_ovr.gif",
-        IMG_BREAKPOINT_MOVED = "icons/moved_ovr.gif",
-        IMG_BREAKPOINT_WARNING = "icons/ovr16/warning_ovr.gif",
-        IMG_BREAKPOINT_ERROR = "icons/ovr16/error_ovr.gif",
-        IMG_BREAKPOINT_OVERLAY = "icons/brkp_ovr.gif",
-    
-        IMG_FOLDER = "icons/full/obj16/fldr_obj.png",
-        IMG_FILE = "icons/full/obj16/file_obj.png";
+        IMG_BREAKPOINT_ENABLED = "icons/full/obj16/brkp_obj",
+        IMG_BREAKPOINT_DISABLED = "icons/full/obj16/brkpd_obj",
+        IMG_BREAKPOINT_INSTALLED = "icons/ovr16/installed_ovr",
+        IMG_BREAKPOINT_CONDITIONAL = "icons/ovr16/conditional_ovr",
+        IMG_BREAKPOINT_MOVED = "icons/moved_ovr",
+        IMG_BREAKPOINT_WARNING = "icons/ovr16/warning_ovr",
+        IMG_BREAKPOINT_ERROR = "icons/ovr16/error_ovr",
+        IMG_BREAKPOINT_OVERLAY = "icons/brkp_ovr",
+
+        IMG_FOLDER = "icons/full/obj16/fldr_obj",
+        IMG_FILE = "icons/full/obj16/file_obj";
 
     private static final Map<String,ImageDescriptor> desc_cache = new HashMap<String,ImageDescriptor>();
     private static final Map<ImageDescriptor,Image> image_cache = new HashMap<ImageDescriptor,Image>();
@@ -87,23 +88,32 @@ public class ImageCache {
         if (name == null) return null;
         ImageDescriptor descriptor = desc_cache.get(name);
         if (descriptor == null) {
-            Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
-            if (bundle != null){
-                URL url = FileLocator.find(bundle, new Path(name), null);
-                if (url != null) descriptor = ImageDescriptor.createFromURL(url);
-            }
-            if (descriptor == null) {
+            String[] ext = { "png", "gif" };
+            for (String e : ext) {
+                IPath path = new Path(name).removeFileExtension().addFileExtension(e);
+                Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
+                if (bundle != null){
+                    URL url = FileLocator.find(bundle, path, null);
+                    if (url != null) {
+                        descriptor = ImageDescriptor.createFromURL(url);
+                        if (descriptor != null) break;
+                    }
+                }
                 bundle = Platform.getBundle("org.eclipse.debug.ui");
                 if (bundle != null){
-                    URL url = FileLocator.find(bundle, new Path(name), null);
-                    if (url != null) descriptor = ImageDescriptor.createFromURL(url);
+                    URL url = FileLocator.find(bundle, path, null);
+                    if (url != null) {
+                        descriptor = ImageDescriptor.createFromURL(url);
+                        if (descriptor != null) break;
+                    }
                 }
-            }
-            if (descriptor == null) {
                 bundle = Platform.getBundle("org.eclipse.cdt.debug.ui");
                 if (bundle != null){
-                    URL url = FileLocator.find(bundle, new Path(name), null);
-                    if (url != null) descriptor = ImageDescriptor.createFromURL(url);
+                    URL url = FileLocator.find(bundle, path, null);
+                    if (url != null) {
+                        descriptor = ImageDescriptor.createFromURL(url);
+                        if (descriptor != null) break;
+                    }
                 }
             }
             if (descriptor == null) {
