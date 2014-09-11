4c5546d03863b23448e6d207a9b0711ac0201b45
Anton Leherbauer <anton.leherbauer@windriver.com>
2014-09-11 11:27:20 +0200

    Bug 443787 - [terminals] SWT Error: "Cannot Initialize Drag" when dragging tabs between views
    Change-Id: I9e6b4918bebb1af69caeadad16441c3399b58d20
    Signed-off-by: Anton Leherbauer <anton.leherbauer@windriver.com>

--- target_explorer/plugins/org.eclipse.tcf.te.ui.terminals/src/org/eclipse/tcf/te/ui/terminals/view/TerminalsView.java
+++ target_explorer/plugins/org.eclipse.tcf.te.ui.terminals/src/org/eclipse/tcf/te/ui/terminals/view/TerminalsView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 Wind River Systems, Inc. and others. All rights reserved.
+ * Copyright (c) 2011, 2014 Wind River Systems, Inc. and others. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -196,6 +196,9 @@ public class TerminalsView extends ViewPart implements ITerminalsView {
 
 				// only for own tab folders
 				if (event.widget != tabFolderControl) return;
+
+				// Skip drag if DnD is still ongoing (bug 443787)
+				if (tabFolderControl.getData(DND.DRAG_SOURCE_KEY) != null) return;
 
 				final CTabFolder draggedFolder = (CTabFolder) event.widget;
 
