fc7b695c5ba063dea27fae58f0726488d4f5fcd2
Anton Leherbauer <anton.leherbauer@windriver.com>
2014-09-23 13:43:47 +0200

    Bug 444598 - Conflicting breakpoint type field editors
    - disable breakpoint type contribution if o.e.cdt.debug.ui >= 7.4
    - remove some obsolete NLS strings

--- plugins/org.eclipse.tcf.cdt.ui/src/org/eclipse/tcf/internal/cdt/ui/breakpoints/CBreakpointTypeFieldEditorFactory.java
+++ plugins/org.eclipse.tcf.cdt.ui/src/org/eclipse/tcf/internal/cdt/ui/breakpoints/CBreakpointTypeFieldEditorFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Wind River Systems and others.
+ * Copyright (c) 2012, 2014 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,13 +12,16 @@ package org.eclipse.tcf.internal.cdt.ui.breakpoints;
 
 import org.eclipse.cdt.debug.core.model.ICBreakpointType;
 import org.eclipse.cdt.debug.ui.breakpoints.IFieldEditorFactory;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.tcf.services.IBreakpoints;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Version;
 
 
 /**
- * HardwareFieldEditorFactory - Create the field editor for hardware breakpoint support.
+ * CBreakpointTypeFieldEditorFactory - Create the field editor for hardware breakpoint support.
  */
 public class CBreakpointTypeFieldEditorFactory implements IFieldEditorFactory {
 
@@ -28,14 +31,31 @@ public class CBreakpointTypeFieldEditorFactory implements IFieldEditorFactory {
     public final static String LABEL_HARDWARE = "Hardware";
     public final static String LABEL_TEMPORARY = "Temporary";
 
+    private final static boolean fgNeedContribution = needContribution();
+
     public FieldEditor createFieldEditor(String name, String labelText, Composite parent) {
-        if (NAME_HARDWARE.equals(name)) {
-            return new CBreakpointTypeFieldEditor (parent, LABEL_HARDWARE, ICBreakpointType.HARDWARE, IBreakpoints.CAPABILITY_BREAKPOINT_TYPE);
-        } else if (NAME_TEMPORARY.equals(name)) {
-            return new CBreakpointTypeFieldEditor (parent, LABEL_TEMPORARY, ICBreakpointType.TEMPORARY, IBreakpoints.CAPABILITY_TEMPORARY);
+        if (fgNeedContribution) {
+            if (NAME_HARDWARE.equals(name)) {
+                return new CBreakpointTypeFieldEditor (parent, LABEL_HARDWARE, ICBreakpointType.HARDWARE, IBreakpoints.CAPABILITY_BREAKPOINT_TYPE);
+            } else if (NAME_TEMPORARY.equals(name)) {
+                return new CBreakpointTypeFieldEditor (parent, LABEL_TEMPORARY, ICBreakpointType.TEMPORARY, IBreakpoints.CAPABILITY_TEMPORARY);
+            }
         }
         return null;
     }
 
+    private static boolean needContribution() {
+        Bundle cdtDebugUi = Platform.getBundle("org.eclipse.cdt.debug.ui");
+        if (cdtDebugUi != null) {
+            int state = cdtDebugUi.getState();
+            if (state == Bundle.ACTIVE || state == Bundle.RESOLVED || state == Bundle.STARTING) {
+                Version version = cdtDebugUi.getVersion();
+                // if cdt.debug.ui version is at least 7.4 (CDT 8.4) we don't need the contribution
+                if (version.compareTo(Version.valueOf("7.4")) >= 0)
+                    return false;
+            }
+        }
+        return true;
+    }
 
 }
--- plugins/org.eclipse.tcf.cdt.ui/src/org/eclipse/tcf/internal/cdt/ui/breakpoints/Messages.java
+++ plugins/org.eclipse.tcf.cdt.ui/src/org/eclipse/tcf/internal/cdt/ui/breakpoints/Messages.java
@@ -15,11 +15,6 @@ public class Messages extends NLS {
     private static final String BUNDLE_NAME = "org.eclipse.tcf.internal.cdt.ui.breakpoints.messages"; //$NON-NLS-1$
 
     public static String TCFThreadFilterQueryExpressionStore;
-    public static String TCFThreadFilterQueryModeButtonState;
-    public static String TCFThreadUseDefaultTriggerScoping;
-    public static String TCFThreadDefaultTriggerScope;
-    public static String TCFThreadFilterQueryButtonBasic;
-    public static String TCFThreadFilterQueryButtonAdvanced;
     public static String TCFThreadFilterQueryButtonEdit;
     public static String TCFThreadFilterQueryAdvancedLabel;
     public static String TCFThreadFilterQueryTreeViewLabel;
@@ -34,13 +29,10 @@ public class Messages extends NLS {
     public static String TCFThreadFilterEditorFormatError;
     public static String TCFThreadFilterEditorUnbalancedParameters;
     public static String TCFThreadFilterEditorNoOpenChannel;
-    public static String TCFThreadFileterEditorInvalidQuery;
     public static String TCFBreakpointPreferencesEnableDefaultTriggerScope;
     public static String TCFBreakpointPreferencesTriggerScopeExpression;
     public static String TCFBreakpointPreferencesDescription;
     public static String TCFBreakpointToggle;
-    public static String TCFBreakpointToggleCannotFindMemory;
-    public static String TCFBreakpointToggleError;
     public static String TCFBreakpointPrefrencesError;
     public static String BreakpointScopeCategory_filter_label;
     public static String BreakpointScopeCategory_contexts_label;
