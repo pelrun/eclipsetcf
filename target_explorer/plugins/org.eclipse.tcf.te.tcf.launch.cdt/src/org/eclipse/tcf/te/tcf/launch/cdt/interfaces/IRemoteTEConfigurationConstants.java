972e35465f3adf0e9708e9920fb904bdfda4e266
Uwe Stieber <uwe.stieber@windriver.com>
2014-10-23 16:51:41 +0200

    Target Explorer: Fix inheritance hierarchy of IRemoteTEConfigurationConstants

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.launch.cdt/src/org/eclipse/tcf/te/tcf/launch/cdt/interfaces/IRemoteTEConfigurationConstants.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.launch.cdt/src/org/eclipse/tcf/te/tcf/launch/cdt/interfaces/IRemoteTEConfigurationConstants.java
@@ -12,36 +12,26 @@
  *******************************************************************************/
 package org.eclipse.tcf.te.tcf.launch.cdt.interfaces;
 
-import org.eclipse.cdt.debug.mi.core.IGDBServerMILaunchConfigurationConstants;
 import org.eclipse.debug.core.DebugPlugin;
 
-public interface IRemoteTEConfigurationConstants extends
-		IGDBServerMILaunchConfigurationConstants {
+public interface IRemoteTEConfigurationConstants {
 
-	public static final String ATTR_REMOTE_CONNECTION = DebugPlugin
-			.getUniqueIdentifier() + ".REMOTE_TCP"; //$NON-NLS-1$
+	public static final String ATTR_REMOTE_CONNECTION = DebugPlugin.getUniqueIdentifier() + ".REMOTE_TCP"; //$NON-NLS-1$
 
-	public static final String ATTR_GDBSERVER_PORT = DebugPlugin
-			.getUniqueIdentifier() + ".ATTR_GDBSERVER_PORT"; //$NON-NLS-1$
-	public static final String ATTR_GDBSERVER_PORT_MAPPED_TO = DebugPlugin
-					.getUniqueIdentifier() + ".ATTR_GDBSERVER_PORT_MAPPED_TO"; //$NON-NLS-1$
-	public static final String ATTR_GDBSERVER_COMMAND = DebugPlugin
-			.getUniqueIdentifier() + ".ATTR_GDBSERVER_COMMAND"; //$NON-NLS-1$
+	public static final String ATTR_GDBSERVER_PORT = DebugPlugin.getUniqueIdentifier() + ".ATTR_GDBSERVER_PORT"; //$NON-NLS-1$
+	public static final String ATTR_GDBSERVER_PORT_MAPPED_TO = DebugPlugin.getUniqueIdentifier() + ".ATTR_GDBSERVER_PORT_MAPPED_TO"; //$NON-NLS-1$
+	public static final String ATTR_GDBSERVER_COMMAND = DebugPlugin.getUniqueIdentifier() + ".ATTR_GDBSERVER_COMMAND"; //$NON-NLS-1$
 
 	public static final String ATTR_GDBSERVER_PORT_DEFAULT = "2345"; //$NON-NLS-1$
 	public static final String ATTR_GDBSERVER_COMMAND_DEFAULT = "gdbserver"; //$NON-NLS-1$
 
 	/*
-	 * Generic Remote Path and Download options ATTR_REMOTE_PATH: Path of the
-	 * binary on the remote. ATTR_SKIP_DOWNLOAD_TO_TARGET: true if download to
-	 * remote is not desired.
+	 * Generic Remote Path and Download options ATTR_REMOTE_PATH: Path of the binary on the remote.
+	 * ATTR_SKIP_DOWNLOAD_TO_TARGET: true if download to remote is not desired.
 	 */
-	public static final String ATTR_REMOTE_PATH = DebugPlugin
-			.getUniqueIdentifier() + ".ATTR_TARGET_PATH"; //$NON-NLS-1$
-	public static final String ATTR_SKIP_DOWNLOAD_TO_TARGET = DebugPlugin
-			.getUniqueIdentifier() + ".ATTR_SKIP_DOWNLOAD_TO_TARGET"; //$NON-NLS-1$
+	public static final String ATTR_REMOTE_PATH = DebugPlugin.getUniqueIdentifier() + ".ATTR_TARGET_PATH"; //$NON-NLS-1$
+	public static final String ATTR_SKIP_DOWNLOAD_TO_TARGET = DebugPlugin.getUniqueIdentifier() + ".ATTR_SKIP_DOWNLOAD_TO_TARGET"; //$NON-NLS-1$
 
-	public static final String ATTR_PRERUN_COMMANDS = DebugPlugin
-			.getUniqueIdentifier() + ".ATTR_PRERUN_CMDS"; //$NON-NLS-1$
+	public static final String ATTR_PRERUN_COMMANDS = DebugPlugin.getUniqueIdentifier() + ".ATTR_PRERUN_CMDS"; //$NON-NLS-1$
 
 }
