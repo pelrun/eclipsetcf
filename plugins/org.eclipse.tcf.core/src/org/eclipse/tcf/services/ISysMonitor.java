be223e40664c941dc7287695dcc03383dcedbb06
Anton Leherbauer <anton.leherbauer@windriver.com>
2015-06-15 15:57:02 +0200

    Bug 470179 - Add SysMonitor context properties "Exe" and "ExeType"
    Change-Id: I7d2e9658aaf30023b622c5d28277754b80cbea31
    Signed-off-by: Anton Leherbauer <anton.leherbauer@windriver.com>

--- plugins/org.eclipse.tcf.core/src/org/eclipse/tcf/services/ISysMonitor.java
+++ plugins/org.eclipse.tcf.core/src/org/eclipse/tcf/services/ISysMonitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2013 Wind River Systems, Inc. and others.
+ * Copyright (c) 2007, 2015 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -234,6 +234,29 @@ public interface ISysMonitor extends IService {
         /** Scheduling policy */
         PROP_POLICY = "Policy";
 
+    /**
+     * Context property extension.
+     * @since 1.4
+     */
+    static final String
+        /** Full path to executable file of the process */
+        PROP_EXE = "Exe",
+
+        /** Executable type (integer). Value is one of:
+         *  0 = user process
+         *  1 = kernel thread
+         *  2 = access denied
+         * See also EXETYPE_* constants */
+        PROP_EXETYPE = "ExeType";
+
+    /**
+     * ExeType property values.
+     * @since 1.4
+     */
+    static final int
+        EXETYPE_USER = 0,
+        EXETYPE_KERNEL = 1,
+        EXETYPE_ACCESS_DENIED = 2;
 
     /**
      * A context corresponds to an execution thread, process, address space, etc.
