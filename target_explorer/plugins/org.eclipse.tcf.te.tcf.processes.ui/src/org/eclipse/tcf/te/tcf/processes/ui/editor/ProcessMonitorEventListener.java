374f7d6f440348e292548082ffaf2f27eec956b6
Uwe Stieber <uwe.stieber@windriver.com>
2015-07-16 07:15:46 +0200

    Target Explorer: Fix copyright

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.processes.ui/src/org/eclipse/tcf/te/tcf/processes/ui/editor/ProcessMonitorEventListener.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.processes.ui/src/org/eclipse/tcf/te/tcf/processes/ui/editor/ProcessMonitorEventListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 Wind River Systems, Inc. and others. All rights reserved.
+ * Copyright (c) 2012, 2015 Wind River Systems, Inc. and others. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -124,6 +124,7 @@ public class ProcessMonitorEventListener extends AbstractEventListener {
 		}
 	}
 
+    @SuppressWarnings("cast")
     protected IPeerNode getPeerNode() {
 		Object element = treeControl.getViewer().getInput();
 		IPeerNode peerNode = element instanceof IPeerNode ? (IPeerNode)element : null;
