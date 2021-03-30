559a8ddee310fa87c1f4e9c1a8bf918b957c2ae9
Eugene Tarassov <eugenet@xilinx.com>
2021-03-30 12:23:20 -0700

    TCF Debugger: fixed: sometimes Cast To Array command is missing in right-click menu

--- plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/commands/CastToArrayCommand.java
+++ plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/commands/CastToArrayCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2016 Wind River Systems, Inc. and others.
+ * Copyright (c) 2009-2011 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,6 @@ package org.eclipse.tcf.internal.debug.ui.commands;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.tcf.internal.debug.ui.Activator;
 import org.eclipse.tcf.internal.debug.ui.ImageCache;
@@ -23,14 +22,16 @@ import org.eclipse.tcf.internal.debug.ui.model.TCFNodeExpression;
 import org.eclipse.tcf.protocol.Protocol;
 import org.eclipse.tcf.services.ISymbols;
 import org.eclipse.tcf.util.TCFDataCache;
+import org.eclipse.tcf.util.TCFTask;
 import org.eclipse.ui.IWorkbenchWindow;
 
 public class CastToArrayCommand extends AbstractActionDelegate {
 
-    private static class CastToTypeInputValidator implements IInputValidator {
+    private String base_type_name;
+    private String cur_length;
+    private TCFNode cur_node;
 
-        public CastToTypeInputValidator() {
-        }
+    private static class CastToTypeInputValidator implements IInputValidator {
 
         public String isValid(String new_text) {
             try {
@@ -59,132 +60,115 @@ public class CastToArrayCommand extends AbstractActionDelegate {
         }
     }
 
-    private static class GetBaseTypeName implements Runnable {
-        final TCFNode node;
-        String base_type_name;
-        String cur_length;
-
-        GetBaseTypeName(TCFNode n) {
-            node = n instanceof ICastToType ? n : null;
-            if (node == null) done();
-            else Protocol.invokeLater(this);
-        }
-
-        public void run() {
-            cur_length = null;
-            if (node instanceof TCFNodeExpression && !((TCFNodeExpression)node).isEnabled()) {
-                done(null);
-                return;
-            }
-            String cast = node.getModel().getCastToType(node.getID());
-            if (cast != null) {
-                if (cast.endsWith("]")) {
-                    int i = cast.lastIndexOf('[');
-                    if (i > 0) {
-                        cur_length = cast.substring(i + 1, cast.length() - 1);
-                        done(cast.substring(0, i));
+    private void getBaseTypeName() {
+        base_type_name = null;
+        cur_length = null;
+        cur_node = getSelectedNode();
+        if (cur_node == null) return;
+        if (!(cur_node instanceof ICastToType)) return;
+        if (cur_node instanceof TCFNodeExpression && !((TCFNodeExpression)cur_node).isEnabled()) return;
+        try {
+            new TCFTask<Boolean>(cur_node.getChannel()) {
+                @Override
+                public void run() {
+                    String cast = cur_node.getModel().getCastToType(cur_node.getID());
+                    if (cast != null) {
+                        if (cast.endsWith("]")) {
+                            int i = cast.lastIndexOf('[');
+                            if (i > 0) {
+                                base_type_name = cast.substring(0, i);
+                                cur_length = cast.substring(i + 1, cast.length() - 1);
+                                done(Boolean.TRUE);
+                                return;
+                            }
+                        }
+                        done(Boolean.FALSE);
                         return;
                     }
-                }
-                done(null);
-                return;
-            }
-            TCFDataCache<ISymbols.Symbol> type_cache = ((ICastToType)node).getType();
-            if (!type_cache.validate(this)) return;
-            ISymbols.Symbol type_data = type_cache.getData();
-            if (type_data == null || type_data.getTypeClass() != ISymbols.TypeClass.pointer) {
-                done(null);
-            }
-            else {
-                for (int i = 0;; i++) {
-                    TCFDataCache<ISymbols.Symbol> base_type_cache = node.getModel().getSymbolInfoCache(type_data.getBaseTypeID());
-                    if (!base_type_cache.validate(this)) return;
-                    ISymbols.Symbol base_type_data = base_type_cache.getData();
-                    if (base_type_data == null) {
-                        done(null);
-                        return;
+                    TCFDataCache<ISymbols.Symbol> type_cache = ((ICastToType)cur_node).getType();
+                    if (!type_cache.validate(this)) return;
+                    ISymbols.Symbol type_data = type_cache.getData();
+                    if (type_data == null || type_data.getTypeClass() != ISymbols.TypeClass.pointer) {
+                        done(Boolean.FALSE);
                     }
                     else {
-                        if (base_type_data.getName() != null) {
-                            done(makePtrTypeName(base_type_data.getName(), i));
-                            return;
-                        }
-                        else if (base_type_data.getTypeClass() == ISymbols.TypeClass.pointer) {
-                            type_data = base_type_data;
-                        }
-                        else if (!base_type_data.getID().equals(base_type_data.getTypeID())) {
-                            // modified type without name, like "volatile int"
-                            base_type_cache = node.getModel().getSymbolInfoCache(base_type_data.getTypeID());
+                        for (int i = 0;; i++) {
+                            TCFDataCache<ISymbols.Symbol> base_type_cache = cur_node.getModel().getSymbolInfoCache(type_data.getBaseTypeID());
                             if (!base_type_cache.validate(this)) return;
-                            base_type_data = base_type_cache.getData();
-                            if (base_type_data != null && base_type_data.getName() != null) {
-                                done(makePtrTypeName(base_type_data.getName(), i));
+                            ISymbols.Symbol base_type_data = base_type_cache.getData();
+                            if (base_type_data == null) {
+                                done(Boolean.FALSE);
+                                return;
                             }
                             else {
-                                done(null);
+                                if (base_type_data.getName() != null) {
+                                    base_type_name = makePtrTypeName(base_type_data.getName(), i);
+                                    done(Boolean.TRUE);
+                                    return;
+                                }
+                                else if (base_type_data.getTypeClass() == ISymbols.TypeClass.pointer) {
+                                    type_data = base_type_data;
+                                }
+                                else if (!base_type_data.getID().equals(base_type_data.getTypeID())) {
+                                    // modified type without name, like "volatile int"
+                                    base_type_cache = cur_node.getModel().getSymbolInfoCache(base_type_data.getTypeID());
+                                    if (!base_type_cache.validate(this)) return;
+                                    base_type_data = base_type_cache.getData();
+                                    if (base_type_data != null && base_type_data.getName() != null) {
+                                        base_type_name = makePtrTypeName(base_type_data.getName(), i);
+                                        done(Boolean.TRUE);
+                                    }
+                                    else {
+                                        done(Boolean.FALSE);
+                                    }
+                                    return;
+                                }
+                                else {
+                                    done(Boolean.FALSE);
+                                    return;
+                                }
                             }
-                            return;
-                        }
-                        else {
-                            done(null);
-                            return;
                         }
                     }
                 }
-            }
-        }
-
-        private String makePtrTypeName(String base, int cnt) {
-            StringBuffer bf = new StringBuffer();
-            bf.append(base);
-            if (cnt > 0) {
-                bf.append(' ');
-                while (cnt > 0) {
-                    bf.append('*');
-                    cnt--;
+                private String makePtrTypeName(String base, int cnt) {
+                    StringBuffer bf = new StringBuffer();
+                    bf.append(base);
+                    if (cnt > 0) {
+                        bf.append(' ');
+                        while (cnt > 0) {
+                            bf.append('*');
+                            cnt--;
+                        }
+                    }
+                    return bf.toString();
                 }
-            }
-            return bf.toString();
+            }.get();
         }
-
-        private void done(String base_type_name) {
-            this.base_type_name = base_type_name;
-            Display.getDefault().asyncExec(new Runnable() {
-                public void run() {
-                    done();
-                }
-            });
-        }
-
-        void done() {
+        catch (Throwable x) {
+            Activator.log(x);
         }
     }
 
     @Override
     protected void run() {
-        new GetBaseTypeName(getSelectedNode()) {
-            void done() {
-                if (base_type_name == null) return;
-                final IWorkbenchWindow window = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow();
-                if (window == null) return;
-                CastToTypeDialog dialog = new CastToTypeDialog(window.getShell(), cur_length);
-                if (dialog.open() != Window.OK) return;
-                final String new_type = dialog.getValue().trim();
-                Protocol.invokeLater(new Runnable() {
-                    public void run() {
-                        node.getModel().setCastToType(node.getID(), base_type_name + "[" + new_type + "]");
-                    }
-                });
+        IWorkbenchWindow window = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow();
+        if (window == null) return;
+        getBaseTypeName();
+        if (base_type_name == null) return;
+        CastToTypeDialog dialog = new CastToTypeDialog(window.getShell(), cur_length);
+        if (dialog.open() != Window.OK) return;
+        final String new_type = dialog.getValue().trim();
+        Protocol.invokeLater(new Runnable() {
+            public void run() {
+                cur_node.getModel().setCastToType(cur_node.getID(), base_type_name + "[" + new_type + "]");
             }
-        };
+        });
     }
 
     @Override
     protected void selectionChanged() {
-        new GetBaseTypeName(getSelectedNode()) {
-            void done() {
-                setEnabled(base_type_name != null);
-            }
-        };
+        getBaseTypeName();
+        setEnabled(base_type_name != null);
     }
 }
