19a259d600c101a8461eb218fcf89f7356572827
Eugene Tarassov <eugenet@xilinx.com>
2022-01-26 15:45:11 -0800

    TCF Debugger: make PeerInfo.attrs public

--- plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/launch/PeerListControl.java
+++ plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/launch/PeerListControl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2020 Wind River Systems, Inc. and others.
+ * Copyright (c) 2011-2022 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -67,9 +67,9 @@ public class PeerListControl implements ISelectionProvider {
     public static class PeerInfo {
         public String id;
         public IPeer peer;
+        public Map<String,String> attrs;
 
         PeerInfo parent;
-        Map<String,String> attrs;
         PeerInfo[] children;
         boolean children_pending;
         Throwable children_error;
